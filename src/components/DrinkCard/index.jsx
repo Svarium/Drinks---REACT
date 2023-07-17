452209d9639b730dccb3d7e3898ddafb5211ad0b
Svarium <you@example.com>
2023-07-17 17:10:41 -0300

    agrego alertas al carrito

--- src/components/DrinkCard/index.jsx
+++ src/components/DrinkCard/index.jsx
@@ -1,64 +1,72 @@
-import React from 'react'
-import { Button, Card, Col } from 'react-bootstrap'
+import React from "react";
+import { Button, Card, Col } from "react-bootstrap";
 import PropTypes from "prop-types";
-import useDrinks from '../../hooks/useDrinks';
+import useDrinks from "../../hooks/useDrinks";
 import styles from "./index.module.css";
-import useCart from '../../hooks/useCart';
-import { types } from '../../types';
+import useCart from "../../hooks/useCart";
+import { types } from "../../types";
+import Swal from "sweetalert2";
 
+export const DrinkCard = ({ drink }) => {
+  const { handleDrinkIdClick } = useDrinks();
 
-export const DrinkCard = ({drink}) => {
-
-  const {handleDrinkIdClick} = useDrinks()
-
-
-  const {dispatch} = useCart();
+  const { dispatch } = useCart();
 
   const handleAddCart = () => {
     dispatch({
-      type:types.addItemToCart,
-      payload: drink
-    })
-  } 
+      type: types.addItemToCart,
+      payload: drink,
+    });
 
+    Swal.fire({
+      title: 'Producto agregado al carrito!',
+      width: 400,
+      padding: '3em',
+      color: '#716add',
+      background: '#fff url()',
+      backdrop: `
+        rgba(0,0,123,0.4)
+        url("https://thumbs.gfycat.com/BouncyWelcomeGrassspider-max-1mb.gif")
+        left top
+        no-repeat
+      `
+    })
+  };
 
   return (
-   <Col md={6} lg={3}>
-   <Card className='mb-4 shadow'>
-    <Card.Img
-    variant='top'
-    src={drink.strDrinkThumb}
-    alt={`Imagen de ${drink.strDrink}`}
-    />
-    <Card.Body>
-        <Card.Title className={styles.strDrink}>
-            {drink.strDrink}
-        </Card.Title>
-    <Button
-    variant={"success"}
-    className='w-100 text-uppercase mt-2'
-    onClick={() => 
-      {handleDrinkIdClick(drink.idDrink)
-     }
-    }
-    >
-        Ver receta
-    </Button>
-    <Button
-    variant={"primary"}
-    className='w-100  text-uppercase mt-2'
-    onClick={handleAddCart}
-    >
-        Comprar
-    </Button>
-    </Card.Body>
-   </Card>
-   </Col>
-  )
-}
+    <Col md={6} lg={3}>
+      <Card className="mb-4 shadow">
+        <Card.Img
+          variant="top"
+          src={drink.strDrinkThumb}
+          alt={`Imagen de ${drink.strDrink}`}
+        />
+        <Card.Body>
+          <Card.Title className={styles.strDrink}>{drink.strDrink}</Card.Title>
+          <Button
+            variant={"success"}
+            className="w-100 text-uppercase mt-2"
+            onClick={() => {
+              handleDrinkIdClick(drink.idDrink);
+            }}
+          >
+            Ver receta
+          </Button>
+          <Button
+            variant={"primary"}
+            className="w-100  text-uppercase mt-2"
+            onClick={handleAddCart}
+          >
+            Comprar
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
 
 DrinkCard.propTypes = {
-    drink : PropTypes.object.isRequired,
+  drink: PropTypes.object.isRequired,
   /*   strDrinkThumb: PropTypes.string.isRequired,
     strDrink: PropTypes.string.isRequired, */
-}
+};
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
 import { CartCanvas } from "../CartCanvas"
 import styles from "./index.module.css"
-import { Button } from "react-bootstrap"
+import { Badge, Button } from "react-bootstrap"
+import useCart from "../../hooks/useCart"
 
 
 export const Header = () => {
@@ -11,15 +12,21 @@ export const Header = () => {
   const handleCloseCart = () => setShowCart(false)
   const handleShowCart = () => setShowCart(true)
 
+  const {cart} = useCart();
+
   return (
     <>
     <header className={` ${styles.header}`}>
     </header>
     <h1 className={`text-shadow text-center ${styles.titulo}`}>18.Drinks</h1>
+    <div>
     <Button className={`m-3 ${styles.buttonCart}`} variant='danger' size='lg' onClick={handleShowCart} >
       <i className="fas fa-shopping-cart fa-lg" ></i>
+      <Badge pill className="btn btn-sm m-1 text-center" bg="warning">{cart.length}</Badge>
       </Button>
-    <CartCanvas
+      
+    </div>
+      <CartCanvas
       showCart={showCart} handleCloseCart={handleCloseCart}
       />
     </>
